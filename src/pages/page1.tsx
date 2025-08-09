c08b3ab73ce78f8777950a3e41243af7c88a52f9
Shaun Underwood <su@sunderwood.co.uk>
2025-08-09 17:24:33 +0100

    View BoundaryButtons

--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BoundariesCard from '../components/BoundariesCard';
 import BoundaryQuestionContainer from '../components/BoundaryQuestionContainer';
+import BoundaryButtons from '../components/BoundaryButtons';
 
 const Page1: React.FC = () => {
   return (
     <div className="w-full max-w-sm space-y-4">
       <BoundariesCard />
       <BoundaryQuestionContainer />
+      <BoundaryButtons />
     </div>
   );
 };
