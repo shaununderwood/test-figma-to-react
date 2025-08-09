3bd6642f6744039f88a0a34ee639ac376c3103ab
Shaun Underwood <su@sunderwood.co.uk>
2025-08-09 17:02:02 +0100

    include BoundaryQuestionContainer

--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BoundariesCard from '../components/BoundariesCard';
+import BoundaryQuestionContainer from '../components/BoundaryQuestionContainer';
 
 const Page1: React.FC = () => {
   return (
     <div className="w-full max-w-sm space-y-4">
       <BoundariesCard />
+      <BoundaryQuestionContainer />
     </div>
   );
 };
